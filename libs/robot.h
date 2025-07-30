a70e97d421c16966693711ab973906cd0c048468
aaryanmakwana <aaryanmakwana2345240@gmail.com>
2025-07-30 07:52:18 +0530

    update

---
+++ libs/robot.h
@@ -0,0 +1,55 @@
+#include <raylib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+Color BackCol = {229,222,211,255};
+Color WhiteCol = {255,255,255,255};
+Color BlackCol = {14,17,17,255};
+Color GreenCol = {14,170,17,80};
+
+struct Robot {
+    Vector2 origin;
+    double link_1_length;
+    double link_2_length;
+    double theta1;
+    double theta2;
+};
+
+bool draw_robot(struct Robot * robot, double theta1, double theta2){
+    robot->theta1 = theta1;
+    robot->theta2 = theta2;
+
+    int pos_x = (int)robot->origin.x;
+    int pos_y = (int)robot->origin.y;
+    int pos_x2 = pos_x + (robot->link_1_length * cos(theta1));
+    int pos_y2 = pos_y + (robot->link_1_length * sin(theta1));
+    int pos_x3 = pos_x2 + (robot->link_2_length * cos(theta2));
+    int pos_y3 = pos_y2 + (robot->link_2_length * sin(theta2));
+
+    DrawCircle(pos_x, pos_y, 20, WhiteCol);
+    DrawCircle(pos_x2, pos_y2, 20, WhiteCol);
+    DrawCircle(pos_x3, pos_y3, 20, WhiteCol);
+    DrawRectangle(pos_x-5,pos_y-5,robot->link_1_length,10,GreenCol);
+    DrawRectangle(pos_x2-5,pos_y2-5,robot->link_2_length,10,GreenCol);
+
+    //printf("%d\t%d\n", pos_x,pos_y);
+    //printf("%d\t%d\n", pos_x2,pos_y2);
+    //printf("%d\t%d\n\n", pos_x3,pos_y3);
+
+    return true;
+}
+
+struct Robot * create_robot(Vector2 origin, double link_1_length, double link_2_length){
+    struct Robot * robot = malloc(sizeof(struct Robot));
+    robot->origin = origin;
+    robot->link_1_length = link_1_length;
+    robot->link_2_length = link_2_length;
+    robot->theta1 = 0;
+    robot->theta2 = 0;
+    printf("robot created!\n");
+    printf("%lf\n", robot->link_1_length);
+    printf("%lf\n", robot->link_1_length);
+
+    return robot;
+}
--- main.c
+++ main.c
@@ -1,10 +1,7 @@
-#include "calc.h"
+#include "libs/calc.h"
+#include "libs/robot.h"
+#include <math.h>
 #include <raylib.h>
-
-Color BackCol = {229,222,211,255};
-Color WhiteCol = {255,255,255,255};
-Color BlackCol = {14,17,17,255};
-Color GreenCol = {14,170,17,80};
 
 void safe_area(){
   for (int ki = 0; ki < 1000; ki++) {
@@ -22,55 +19,35 @@ int main(){
   InitWindow(1000,1000,"SCARA");
   SetTargetFPS(60);
 
-  B2.x = 250.0;
-  B2.y = 650.0;
-  B1.x = 250.0;
-  B1.y = 350.0;
-  double THETA_2 = 0;
-
-  struct position EF;
-  struct position TARGET;
-
-  TARGET.x = 600;
-  TARGET.y = 500;
 
   int TARGETx=0;
   int TARGETy=0;
+
+  double THETA_2 = 0.5;
+  double THETA_1 = 0.3;
+
+  struct Robot *rb1 = create_robot((Vector2){500,500}, 200, 200);
 
   SetWindowState(FLAG_WINDOW_RESIZABLE);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BlackCol);
-    reverse_K(TARGET.x, TARGET.y);
-    THETA_2 = forward_K2(THETA_1, PHI_1);
-    //safe_area();
+
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
       Vector2 ballPosition = GetMousePosition();
       //printf("%f\n", ballPosition.x);
-      TARGET.x = ballPosition.x;
-      TARGET.y = ballPosition.y;
+      TARGETx = ballPosition.x;
+      TARGETy = ballPosition.y;
+
     }
 
+    DrawRectangle(TARGETx-5,TARGETy-5,10,10,GreenCol);
+    draw_robot(rb1, THETA_1, THETA_2);
+    THETA_1 += 0.02;
+    THETA_2 += 0.01;
 
-    DrawCircleLines(L1.x, L1.y, L1b, DARKBLUE);
-    DrawCircleLines(L2.x, L2.y, L2b, DARKBLUE);
-    DrawCircleLines(B1.x, B1.y, L1a, BackCol);
-    DrawCircleLines(B2.x, B2.y, L2a, BackCol);
 
-    DrawCircle(B1.x, B1.y, 20, WhiteCol);
-    DrawCircle(B2.x, B2.y, 20, WhiteCol);
-    DrawRectangle(L1.x-5,L1.y-5,10,10,WhiteCol);
-    DrawRectangle(L2.x-5,L2.y-5,10,10,WhiteCol);
-    DrawRectangle(EF.x-5,EF.y-5,10,10,BackCol);
-    DrawRectangle(TARGET.x-5,TARGET.y-5,10,10,GreenCol);
-
-    DrawLine(B1.x, B1.y, L1.x, L1.y, WhiteCol);
-    DrawLine(B2.x, B2.y, L2.x, L2.y, WhiteCol);
-    EF.x = L1.x+L1b*cos(THETA_2);
-    EF.y = L1.y+L1b*sin(THETA_2);
-    DrawLine(L1.x,L1.y,EF.x,EF.y,WhiteCol);
-    DrawLine(L2.x,L2.y,EF.x,EF.y,WhiteCol);
     EndDrawing();
 
     /*TARGETx += 1;
